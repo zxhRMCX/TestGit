87ebb126338da8d7a59d81f25a770104b2b7afd0
zxhRMCX <2583111340@qq.com>
2019-12-18 08:33:26 +0800

    add "private String weclcome."

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,11 @@
 public class HelloWorld {
+    private String hello;
+    
+    public String getHello() {
+        return hello;
+    }
 
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
 }
